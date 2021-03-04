38c1de80bf7b38d20d68b22b1dc456e58c916a26
Dearkano <vaynetian@gmail.com>
2021-03-04 00:30:45 -0500

    fix: delete key

--- Server/config/azure.js
+++ Server/config/azure.js
@@ -1,8 +1,8 @@
+var config = require("../db.json");
 var azureConfig = {};
 
 azureConfig.endpoint = "https://digitalag21.documents.azure.com:443/";
-azureConfig.key =
-  "WJf28ZPeIwBcSexKEaDcqbT2WkbN00517CN1vZyd4C1Ve7uUYZ8f8SG5bnC0kxWPtNpsA9Ac7i4qNQJjrmNelw==";
+azureConfig.key = config.azure_key;
 
 azureConfig.database = {
   id: "ToDoList",
@@ -10,81 +10,6 @@ azureConfig.database = {
 
 azureConfig.container = {
   id: "DOCUMENTCOLLECTIONID",
-};
-
-azureConfig.items = {
-  Andersen: {
-    id: "Anderson.1",
-    Country: "USA",
-    lastName: "Andersen",
-    parents: [
-      {
-        firstName: "Thomas",
-      },
-      {
-        firstName: "Mary Kay",
-      },
-    ],
-    children: [
-      {
-        firstName: "Henriette Thaulow",
-        gender: "female",
-        grade: 5,
-        pets: [
-          {
-            givenName: "Fluffy",
-          },
-        ],
-      },
-    ],
-    address: {
-      state: "WA",
-      county: "King",
-      city: "Seattle",
-    },
-  },
-  Wakefield: {
-    id: "Wakefield.7",
-    Country: "Italy",
-    parents: [
-      {
-        familyName: "Wakefield",
-        firstName: "Robin",
-      },
-      {
-        familyName: "Miller",
-        firstName: "Ben",
-      },
-    ],
-    children: [
-      {
-        familyName: "Merriam",
-        firstName: "Jesse",
-        gender: "female",
-        grade: 8,
-        pets: [
-          {
-            givenName: "Goofy",
-          },
-          {
-            givenName: "Shadow",
-          },
-        ],
-      },
-      {
-        familyName: "Miller",
-        firstName: "Lisa",
-        gender: "female",
-        grade: 1,
-      },
-    ],
-    address: {
-      state: "NY",
-      county: "Manhattan",
-      city: "NY",
-    },
-    isRegistered: false,
-  },
 };
 
 module.exports = azureConfig;
