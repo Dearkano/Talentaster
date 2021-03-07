67ea6fbfeeba83bf91d2bf654c3d2b3cb7542ab9
Dearkano <vaynetian@gmail.com>
2021-03-07 10:46:48 -0500

    update

--- App/routes/result.js
+++ App/routes/result.js
@@ -17,25 +17,31 @@ import {
   VictoryTheme,
   VictoryBar,
 } from "victory-native";
+import StarRating from "react-native-star-rating";
 
 export default function Result({ route, navigation }) {
   const { data, photo } = route.params;
-  const sampleData1 = [
-    { x: "plumbness", y: data.plumbness },
-    { x: "fibrousness", y: data.fibrousness },
-    { x: "crispness", y: data.crispness },
-    { x: "sweetness", y: data.sweetness },
-    { x: "sourness", y: data.sourness },
+  const sampleData = [
+    { x: "plumbness", y: data.plumpness },
+    { x: "PH", y: data.acidity },
+    { x: "moisture", y: data.moisture },
+    { x: "hardness", y: data.crispness },
+    { x: "soluble solid", y: data.soluble },
+    { x: "cellulose", y: data.cellulose },
   ];
 
-  const sampleData2 = [
-    { x: "moisture", y: 50 },
-    { x: "sugar", y: 20 },
-    { x: "vitamins", y: 15 },
-    { x: "acidity", y: 30 },
-    { x: "cellulose", y: 70 },
-    { x: "soluble solids", y: 30 },
-  ];
+  let sweetness = 5;
+  let sourness = 5;
+  let ripeness = 5;
+  if (data.soluble < 0.8) {
+    sweetness = 3;
+  }
+  if (data.acidity < 0.8) {
+    sourness = 3;
+  }
+  if (data.crispness < 0.8) {
+    ripeness = 3;
+  }
 
   let width = 0;
   let height = 0;
@@ -50,6 +56,9 @@ export default function Result({ route, navigation }) {
     height = (300 / photo.height) * photo.width;
   }
   fruit = data.tag;
+  if (data.tag === "") {
+    navigation.navigate("Home");
+  }
   return (
     <ScrollView style={styles.container}>
       <View
@@ -67,24 +76,115 @@ export default function Result({ route, navigation }) {
           }}
         />
       </View>
-      <View>
-        <Text>{fruit}</Text>
+      <View
+        style={{
+          alignSelf: "center",
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 45,
+            fontFamily: "Arial",
+            fontWeight: "bold",
+            color: "#ff9933",
+            marginBottom: 50,
+          }}
+        >
+          {fruit}
+        </Text>
+      </View>
+      <View
+        style={{
+          width: 300,
+          alignSelf: "center",
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 25,
+            fontFamily: "Arial",
+            fontWeight: "bold",
+            color: "#5ccccb",
+            alignSelf: "center",
+          }}
+        >
+          Sweetness
+        </Text>
+        <StarRating
+          disabled={false}
+          maxStars={5}
+          rating={sweetness}
+          emptyStar={"ios-star-outline"}
+          fullStar={"ios-star"}
+          halfStar={"ios-star-half"}
+          iconSet={"Ionicons"}
+          fullStarColor={"#5ccccb"}
+        />
+      </View>
+      <View
+        style={{
+          width: 300,
+          alignSelf: "center",
+          marginTop: 30,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 25,
+            fontFamily: "Arial",
+            fontWeight: "bold",
+            color: "#dd9cac",
+            alignSelf: "center",
+          }}
+        >
+          Sourness
+        </Text>
+        <StarRating
+          disabled={false}
+          maxStars={5}
+          rating={sourness}
+          emptyStar={"ios-star-outline"}
+          fullStar={"ios-star"}
+          halfStar={"ios-star-half"}
+          iconSet={"Ionicons"}
+          fullStarColor={"#dd9cac"}
+        />
+      </View>
+      <View
+        style={{
+          width: 300,
+          alignSelf: "center",
+          marginTop: 30,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 25,
+            fontFamily: "Arial",
+            fontWeight: "bold",
+            color: "#f29d7a",
+            alignSelf: "center",
+          }}
+        >
+          Ripeness
+        </Text>
+        <StarRating
+          disabled={false}
+          maxStars={5}
+          rating={ripeness}
+          emptyStar={"ios-star-outline"}
+          fullStar={"ios-star"}
+          halfStar={"ios-star-half"}
+          iconSet={"Ionicons"}
+          fullStarColor={"#f29d7a"}
+        />
       </View>
       <View style={styles.column}>
         <View style={styles.chart}>
           <VictoryChart polar theme={VictoryTheme.material} width={300}>
             <VictoryArea
-              data={sampleData1}
+              data={sampleData}
               style={{ data: { fill: "#c43a31" } }}
-            />
-            <VictoryPolarAxis />
-          </VictoryChart>
-        </View>
-        <View style={styles.chart}>
-          <VictoryChart polar theme={VictoryTheme.material} width={300}>
-            <VictoryArea
-              data={sampleData2}
-              style={{ data: { fill: "#74b0dd" } }}
             />
             <VictoryPolarAxis />
           </VictoryChart>
@@ -147,5 +247,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: 100,
     transform: [{ rotateZ: "-90deg" }],
+  },
+  myStarStyle: {
+    color: "yellow",
+    backgroundColor: "transparent",
+    textShadowColor: "black",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 2,
+  },
+  myEmptyStarStyle: {
+    color: "white",
   },
 });
